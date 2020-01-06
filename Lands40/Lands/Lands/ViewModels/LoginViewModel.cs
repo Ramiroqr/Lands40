79c3310e85b81ab0a4545bc3b1955e10d52df0ce
Ramiroqr <ramiroquisper@gmail.com>
2020-01-06 09:16:03 -0500

    LandsViewModel

---
+++ Lands40/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,116 @@
+﻿namespace Lands.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using Lands.Views;
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    class LoginViewModel : BaseViewModel
+    {
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+
+        public bool IsRememberme
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRememberme = true;
+            this.IsEnabled = true;
+
+            this.Email = "ram@gmail.com";
+            this.Password = "123";
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get { return new RelayCommand(Login); }
+        }
+        #endregion
+
+        #region Methods
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Email...",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Password...",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            if (this.Email != "ram@gmail.com" || this.Password != "123")
+            {
+                this.IsRunning = true;
+                this.IsEnabled = false;
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or Password incorrect...",
+                    "Accept");
+                this.Email = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            this.Email = string.Empty;
+            this.Password = string.Empty;
+
+            MainViewModel.GetInstance().Lands = new LandsViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new LandsPage());
+        }
+        #endregion
+    }
+}
--- Lands40/Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands40/Lands/Lands/ViewModels/MainViewModel.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lands.ViewModels
+﻿namespace Lands.ViewModels
 {
     class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
 
+        public LandsViewModel Lands
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Patron Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+                return new MainViewModel();
+
+            return instance;
+        }
+        #endregion
     }
 }
